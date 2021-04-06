6ae2a4345d0f91ff937be69944e4a36d5569d55c
gizemsacal <46420726+gizemsacal@users.noreply.github.com>
2021-04-06 16:14:14 +0300

    MethodExample

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("total ="+ stringT("Gizem","Saçal"));
+
+
+    }
+
+     public static String stringT(String sumString, String sumString2){
+        String total;
+        total=sumString+ sumString2;
+
+        return total;
+
+
+
+}
+}
